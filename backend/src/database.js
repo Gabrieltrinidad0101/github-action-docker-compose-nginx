fa36e4fc9e652af93657c4521d99379242d396fc
Gabrieltrinidad0101 <gabrielqwes123@gmail.com>
2023-03-08 21:31:37 -0400

    fix: url in mongodb

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost/hexagonalApp")
+mongoose.connect("mongodb://mongodb/hexagonalApp")
     .then(_=> console.log("db is connected"))
     .catch(error => console.error(error))
